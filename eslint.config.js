ba4368c5a94ffb6931bbe3c5a8b01bc477f2fa0c
JohnLkt <john.lukito@gmail.com>
2026-07-24 00:08:09 +0700

    chore: update ignores

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import prettierPlugin from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/components/ui/**', 'src/lib/utils.ts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
